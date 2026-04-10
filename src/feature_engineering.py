cb689c74a0048e379bafae087f75aa29c9796d4a
riddhikale <riddhiskale28@gmail.com>
2026-04-10 20:32:13 +0530

    refactor player feature engineering in src module

---
+++ src/feature_engineering.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+#batting stats
+def compute_batting_stats(deliveries):
+
+    player_runs = deliveries.groupby("batter")["batsman_runs"].sum()
+
+    balls_faced = deliveries.groupby("batter").size()
+
+    dismissals = deliveries[deliveries["is_wicket"] == 1] \
+        .groupby("player_dismissed").size()
+
+    batting_stats = pd.DataFrame({
+        "total_runs": player_runs,
+        "balls_faced": balls_faced
+    })
+
+    batting_stats["dismissals"] = dismissals
+
+    batting_stats.fillna(0, inplace=True)
+
+    batting_stats["batting_average"] = (
+        batting_stats["total_runs"] / batting_stats["dismissals"].replace(0,1)
+    )
+
+    batting_stats["strike_rate"] = (
+        batting_stats["total_runs"] / batting_stats["balls_faced"]
+    ) * 100
+
+    return batting_stats
+
+
+#bowling stats
+def compute_bowling_stats(deliveries):
+
+    wickets = deliveries.groupby("bowler")["is_wicket"].sum()
+
+    runs_conceded = deliveries.groupby("bowler")["total_runs"].sum()
+
+    balls_bowled = deliveries.groupby("bowler").size()
+
+    bowling_stats = pd.DataFrame({
+        "total_wickets": wickets,
+        "runs_conceded": runs_conceded,
+        "balls_bowled": balls_bowled
+    })
+
+    bowling_stats["overs"] = bowling_stats["balls_bowled"] / 6
+
+    bowling_stats["bowling_economy"] = (
+        bowling_stats["runs_conceded"] / bowling_stats["overs"]
+    )
+
+    bowling_stats["bowling_average"] = (
+        bowling_stats["runs_conceded"] / bowling_stats["total_wickets"].replace(0,1)
+    )
+
+    return bowling_stats
